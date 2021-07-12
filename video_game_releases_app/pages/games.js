bef7abce5f11056dd18a0f3b7319bfad36e23188
Khalil <khalilfoulks@gmail.com>
2021-07-12 14:58:43 -0400

    added placeholder for games list

---
+++ video_game_releases_app/pages/games.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+const games = () => {
+    return (
+        <div>
+            <Head>
+                <title>2021 Upcoming Games</title>
+                <div>
+                    List of 2021 games goes here
+                </div>
+            </Head>
+        </div>
+    )
+}
+
+export default games
