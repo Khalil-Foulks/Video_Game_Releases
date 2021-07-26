c53078383353ba06a93a6002e59c0561f8d2e706
Khalil <khalilfoulks@gmail.com>
2021-07-26 16:30:24 -0400

    corrected authorization header

--- video_game_releases_app/next.config.js
+++ video_game_releases_app/next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    client_id: 'yo6ss1dhbtar2wihvnn844h7hxjget',
+    client_secret: 'hv4nezdmttnzumbeiytiyxumm2rbpf',
+    grant_type: 'client_credentials',
+    Authorization: `Bearer 8qrw9kjtiv0ufvq5g3zrsqvlj3rnq5`
+  },
 }
