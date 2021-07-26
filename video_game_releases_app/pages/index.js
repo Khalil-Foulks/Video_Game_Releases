6760f7173ab7e4166c65c2a07080a87966752a37
Khalil <khalilfoulks@gmail.com>
2021-07-26 16:32:06 -0400

    corrected request to API

--- video_game_releases_app/pages/index.js
+++ video_game_releases_app/pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
+import axiosWithAuth from "../Utils/axiosWithAuth"
 import styles from '../styles/Home.module.css'
 import Games from './games'
 
-export default function Home() {
+export default function Home({dates}) {
+  console.log(dates)
   return (
     <div className={styles.container}>
       <Games />
@@ -10,3 +12,22 @@ export default function Home() {
   )
 }
 
+
+export const getStaticProps = async () => {  
+  let res = "nothing"
+
+  await axiosWithAuth()
+  .post("/release_dates")
+  .then((response) => {
+    console.log("Release Dates", response)
+    res = response.data
+  })
+
+  const dates = await res
+
+  return {
+    props: {
+      dates
+    }
+  }
+}
