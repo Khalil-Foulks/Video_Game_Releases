8f65182409ac210c1bb38023883a59a56a64cd31
Khalil <khalilfoulks@gmail.com>
2021-07-26 17:58:41 -0400

    changed config to use .env.local

--- video_game_releases_app/next.config.js
+++ video_game_releases_app/next.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    client_id: 'yo6ss1dhbtar2wihvnn844h7hxjget',
-    client_secret: 'hv4nezdmttnzumbeiytiyxumm2rbpf',
+    client_id: process.env.client_id,
+    client_secret: process.env.client_secret,
     grant_type: 'client_credentials',
-    Authorization: `Bearer 8qrw9kjtiv0ufvq5g3zrsqvlj3rnq5`
+    Authorization: process.env.Authorization
   },
 }
